45c3b92311832751e77b8c2c4dfe320e5a9ece3b
norisgore <rutuno01@gmail.com>
2021-12-19 20:15:27 +0300

    code quality update

--- src/apiRequests.js
+++ src/apiRequests.js
@@ -1,47 +1,13 @@
 import translate from "translate";
 
-function getQuoteByTheme(theme, translated = false) {
-  if (!theme) {
+function getQuoteByTheme(insertedTheme, translated = false) {
+  if (!insertedTheme) {
     return;
   }
 
-  console.log("working");
-  if (!translated) {
-    return translate(theme, { from: "ru", to: "en" })
-      .then((translatedTheme) =>
-        fetch(
-          `https://api.quotable.io/search/quotes?query=${translatedTheme
-            .trim()
-            .toLowerCase()}&fields=content,author&limit=150`,
-          {
-            method: "GET",
-          }
-        )
-          .then((response) => {
-            return response.json();
-          })
-          .then((response) => {
-            let quoteObj =
-              response.results[Math.round(Math.random() * response.totalCount)];
-            return Promise.all([
-              translate(quoteObj.content, {
-                to: "ru",
-              }),
-              translate(quoteObj.author, {
-                to: "ru",
-              }),
-            ]);
-          })
-      )
-      .catch(() => {
-        alert(
-          "Если ты это видишь, то, наверное слишком много пользовалась моим сайтиком и теперь нужно подождать) (или же я накосячил и это просто ошибка)"
-        );
-        return;
-      });
-  } else {
+  function quoteFetch(translatedTheme) {
     return fetch(
-      `https://api.quotable.io/search/quotes?query=${theme
+      `https://api.quotable.io/search/quotes?query=${translatedTheme
         .trim()
         .toLowerCase()}&fields=content,author&limit=150`,
       {
@@ -69,6 +35,16 @@ function getQuoteByTheme(theme, translated = false) {
         );
         return;
       });
+  }
+
+  if (!translated) {
+    return translate(insertedTheme, { from: "ru", to: "en" }).then(
+      (translatedTheme) => {
+        return quoteFetch(translatedTheme);
+      }
+    );
+  } else {
+    return quoteFetch(insertedTheme);
   }
 }
 
